e561fd43cf66252e264d56fc67bedf5a101c8c05
Fabian Arancibia <37383229+FabianArancibiaM@users.noreply.github.com>
2018-07-15 18:57:19 -0400

    modif

--- ExamenPhp/manejadorAnalisisMuestra.php
+++ ExamenPhp/manejadorAnalisisMuestra.php
@@ -3,14 +3,14 @@
     if(!isset($_REQUEST['accion']))
     {
         require_once "controllers/AnalisisMuestraController.php";
-        $analisisMuestra = new AnalisisMuestra();
+        $analisisMuestra = new AnalisisMuestraController();
         $analisisMuestra->buscarTodas();
     }
     else
     {
         $accion = $_REQUEST['accion'];
         require_once "controllers/AnalisisMuestraController.php";
-        $analisisMuestra = new AnalisisMuestra();
+        $analisisMuestra = new AnalisisMuestraController();
         
         // Llama la accion
         call_user_func( array( $analisisMuestra, $accion ) );
--- ExamenPhp/manejadorAnalisisResultado.php
+++ ExamenPhp/manejadorAnalisisResultado.php
@@ -3,14 +3,14 @@
     if(!isset($_REQUEST['accion']))
     {
         require_once "controllers/AnalisisResultadoController.php";
-        $analisisResultado = new AnalisisResultado();
+        $analisisResultado = new AnalisisResultadoController();
         $analisisResultado->buscarTodas();
     }
     else
     {
         $accion = $_REQUEST['accion'];
         require_once "controllers/AnalisisResultadoController.php";
-        $analisisResultado = new AnalisisResultado();
+        $analisisResultado = new AnalisisResultadoController();
         
         // Llama la accion
         call_user_func( array( $analisisResultado, $accion ) );
