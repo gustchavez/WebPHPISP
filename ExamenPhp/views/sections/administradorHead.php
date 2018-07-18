4344b76a4fa3477b48b503eea61fbee541cf1b4d
Diego <dgtvillalobos@gmail.com>
2018-07-17 20:36:27 -0400

    CAMBIOS

--- ExamenPhp/views/sections/administradorHead.php
+++ ExamenPhp/views/sections/administradorHead.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<title>Dashboard</title>
+	<title>ADMINISTRADOR</title>
 	<link rel="stylesheet" href="../../WebPHPISP/views/css/bootstrap.min.css">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Roboto:300,400,500" rel="stylesheet">
 	<link rel="stylesheet" href="../../WebPHPISP/views/css/fontello.css">
@@ -14,15 +14,12 @@
 		<div class="row">
 			<div class="barra-lateral col-12 col-sm-auto">
 				<div class="logo">
-					<h2>CLIENTE</h2>
+					<h2>Dashboard</h2>
 				</div>
 				<nav class="menu d-flex d-sm-block justify-content-center flex-wrap">
-					<a href="manejadorParticular.php?accion=crudEditarPerfil&codigo= <?php $_REQUEST['codigo']?> "><i class="icon-home"></i><span>Muestras</span></a>
+					<a href="#"><i class="icon-home"></i><span>Muestras</span></a>
 					<a href="#"><i class="icon-doc-text"></i><span>Resultados Análisis</span></a>
 					<a href="#"><i class="icon-users"></i><span>Modificar Perfil</span></a>
 					<a href="#"><i class="icon-logout"></i><span>Salir</span></a>
 				</nav>
 			</div>
-
-
-			  ?>
